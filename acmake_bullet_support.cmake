04609cef1c2a9fab670e5c7e26e52330c374f6b3
answeror <answeror@gmail.com>
2012-06-05 15:30:02 +0800

    bullet no target call; use macro instead of function to expose ANS_INCLUDE_DIRS

--- acmake_bullet_support.cmake
+++ acmake_bullet_support.cmake
@@ -1,11 +1,14 @@
 include(acmake_common)
 include(acmake_env_home)
 
-# usage: acmake_bullet_support(<target>)
+# usage: acmake_bullet_support([<target>])
+#   if target is not specified, no linking
 # requires: environment variable BULLET_HOME or BULLET_ROOT
-macro(acmake_bullet_support TARGET)
+macro(acmake_bullet_support)
     acmake_env_home(BULLET_ROOT BULLET)
     find_package(Bullet REQUIRED)
     include_directories(${BULLET_INCLUDE_DIRS})
-    target_link_libraries(${TARGET} ${BULLET_LIBRARIES})
+    if(ARGN)
+        target_link_libraries(${ARGN} ${BULLET_LIBRARIES})
+    endif()
 endmacro()
