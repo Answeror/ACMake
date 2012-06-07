d56c93065e34159df81da844e34315a0d12e0d99
answeror <answeror@gmail.com>
2012-06-07 16:28:08 +0800

    add acmake_assert_defined; fix acmake_bullet_support linking

--- acmake_bullet_support.cmake
+++ acmake_bullet_support.cmake
@@ -1,5 +1,6 @@
 include(acmake_common)
 include(acmake_env_home)
+#include(acmake_parse_arguments)
 
 # usage: acmake_bullet_support([<target>])
 #   if target is not specified, no linking
@@ -8,7 +9,11 @@ macro(acmake_bullet_support)
     acmake_env_home(BULLET_ROOT BULLET)
     find_package(Bullet REQUIRED)
     include_directories(${BULLET_INCLUDE_DIRS})
-    if(ARGN)
-        target_link_libraries(${ARGN} ${BULLET_LIBRARIES})
+    if(${ARGC} GREATER 0)
+        acmake_bullet_support_link(${ARGN})
     endif()
 endmacro()
+
+function(acmake_bullet_support_link TARGET)
+    target_link_libraries(${TARGET} ${BULLET_LIBRARIES})
+endfunction()
