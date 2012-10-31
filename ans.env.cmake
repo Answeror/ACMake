d420d82f4f156ed747e4a247674fa728b1a1156b
answeror <answeror@gmail.com>
2012-10-31 19:09:58 +0800

    add cppunit finder, from OGRE; remove default SDK path

--- ans.env.cmake
+++ ans.env.cmake
@@ -1,14 +1,14 @@
 if(NOT ANS_ENV_INCLUDED)
     set(ANS_TEMPLATE_DIR "${ACMake_INSTALL_DIR}/template")
     set(ANS_ENV_INCLUDED TRUE)
-    set(CMLEX_FIND_PACKAGE_HINTS ${CMAKE_SOURCE_DIR}/misc/3rd)
+    #set(CMLEX_FIND_PACKAGE_HINTS ${CMAKE_SOURCE_DIR}/misc/3rd)
 
-    # default sdk path
-    if(NOT ACMAKE_SDK_PATH)
-        file(TO_CMAKE_PATH $ENV{PROGRAMFILES} ACMAKE_SDK_PATH)
-        set(ACMAKE_SDK_PATH "${ACMAKE_SDK_PATH}/sdk")
-    endif()
+    ## default sdk path
+    #if(NOT ACMAKE_SDK_PATH)
+        #file(TO_CMAKE_PATH $ENV{PROGRAMFILES} ACMAKE_SDK_PATH)
+        #set(ACMAKE_SDK_PATH "${ACMAKE_SDK_PATH}/sdk")
+    #endif()
 
     # find package hints
-    set(ACMAKE_FIND_PACKAGE_HINTS ${CMAKE_SOURCE_DIR}/misc/3rd)
+    #set(ACMAKE_FIND_PACKAGE_HINTS ${CMAKE_SOURCE_DIR}/misc/3rd)
 endif()
