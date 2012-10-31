2ec87ae4997970cd724287d3134253b86768913e
answeror <answeror@gmail.com>
2012-10-31 17:08:49 +0800

    XXX_HOME will be considered in acmake_simple_support

--- acmake_simple_support.cmake
+++ acmake_simple_support.cmake
@@ -3,9 +3,17 @@
 include(acmake_common)
 
 # usage: acmake_simple_support(<target> <library>)
+# 
+# XXX_HOME will be considered
 macro(acmake_simple_support TARGET LIBRARY)
-    find_package(${LIBRARY} REQUIRED)
     string(TOUPPER ${LIBRARY} UPPER_LIBRARY)
+    if(EXISTS "${${LIBRARY}_HOME}")
+        list(APPEND CMAKE_PREFIX_PATH "${${LIBRARY}_HOME}")
+    endif()
+    if(EXISTS "${${UPPER_LIBRARY}_HOME}")
+        list(APPEND CMAKE_PREFIX_PATH "${${UPPER_LIBRARY}_HOME}")
+    endif()
+    find_package(${LIBRARY} REQUIRED)
     include_directories(${${UPPER_LIBRARY}_INCLUDE_DIRS})
     target_link_libraries(${TARGET} ${${UPPER_LIBRARY}_LIBRARIES})
 endmacro()
