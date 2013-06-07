f473ba06e07b9ed191a31ea43f2b6657cde81fa6
answeror <answeror@gmail.com>
2013-06-07 21:43:00 +0800

    add dynamic loaded dll copy

--- acmake_copy_dependencies.cmake
+++ acmake_copy_dependencies.cmake
@@ -1,6 +1,8 @@
 set(ACMAKE_FIXUP_PATH ${CMAKE_CURRENT_LIST_DIR}/acmake_fixup.cmake.in)
 macro(acmake_copy_dependencies TARGET)
     acmake_get_target_property(RUNTIME_DIRS ${TARGET} ACMAKE_RUNTIME_DIRS)
+    acmake_get_target_property(RUNTIME_DEBUG ${TARGET} ACMAKE_RUNTIME_DEBUG)
+    acmake_get_target_property(RUNTIME_RELEASE ${TARGET} ACMAKE_RUNTIME_RELEASE)
     configure_file(
         ${ACMAKE_FIXUP_PATH}
         ${CMAKE_CURRENT_BINARY_DIR}/acmake_fixup.cmake
@@ -11,6 +13,7 @@ macro(acmake_copy_dependencies TARGET)
         POST_BUILD
         COMMAND ${CMAKE_COMMAND}
             -DTARGET_FILE_PATH=$<TARGET_FILE:${TARGET}>
+            -DCMAKE_BUILD_TYPE=$<CONFIGURATION>
             -P "${CMAKE_CURRENT_BINARY_DIR}/acmake_fixup.cmake"
         VERBATIM
     )
