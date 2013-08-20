2429d2c6766b12d7876f86797b1bc5eb225cd449
answeror <answeror@gmail.com>
2013-08-20 10:38:46 +0800

    use tar

--- acmake_pack.cmake
+++ acmake_pack.cmake
@@ -6,16 +6,11 @@ set(ACMAKE_PACK_INCLUDED TRUE)
 include(acmake_version)
 
 macro(acmake_pack TARGET)
-    find_program(ACMAKE_SEVENZ 7z)
-    if(NOT ACMAKE_SEVENZ)
-        message("Can't find 7z, packaging failed.")
-    else()
-        acmake_version(${TARGET})
-        add_custom_target(
-            pack
-            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_BINARY_DIR}/install ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}
-            COMMAND ${ACMAKE_SEVENZ} a -y -sfx7z.sfx ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}.exe ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}
-            VERBATIM
-            )
-    endif()
+    acmake_version(${TARGET})
+    add_custom_target(
+        pack
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_BINARY_DIR}/install ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}
+        COMMAND ${CMAKE_COMMAND} -E tar czf ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}.tar.gz ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}
+        VERBATIM
+        )
 endmacro()
