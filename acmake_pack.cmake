421f6a9a8188e0cf8565e9c8fe64f62a9e6d55ff
answeror <answeror@gmail.com>
2014-06-22 15:03:11 +0800

    use CMAKE_INSTALL_PREFIX to pack

--- acmake_pack.cmake
+++ acmake_pack.cmake
@@ -9,7 +9,7 @@ macro(acmake_pack TARGET)
     acmake_version(${TARGET})
     add_custom_target(
         pack
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_BINARY_DIR}/install ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_INSTALL_PREFIX} ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}
         COMMAND ${CMAKE_COMMAND} -E tar czf ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}.tar.gz ${CMAKE_BINARY_DIR}/${TARGET}.${${TARGET}_FILE_VERSION}
         VERBATIM
         )
